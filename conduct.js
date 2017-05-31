d99230128867610c34763008a45efbc05e4ab087
Adam Miller <babaganoosh76@gmail.com>
2017-05-31 16:06:13 -0700

    Added bouncing animation

--- conduct.js
+++ conduct.js
@@ -2,9 +2,16 @@ var b;
 var play = true;
 var change = true;
 var newSig = 4;
+var click;
+
+click = new Audio();
+// click = new Audio('media/shine.wav');
 
 function setup() {
-	createCanvas(300, 300).parent('innerLeft');
+	var cw = $("#canvas").width();
+	var ch = $("#canvas").height();
+	console.log(cw, ch);
+	createCanvas(cw, cw).parent('canvas');
 	b = new Baton();
 }
 
@@ -23,11 +30,10 @@ function Baton() {
 	this.beat = 4;
 	this.tempo = 120;
 	this.inc = 1;
-	var rad = height/8, vely = 0, count = 0;
+	var rad = height/16, vely = 0, count = 0;
 
 	this.update = function(){
 		//CHANGE TIME SIGNATURE
-		console.log(vely);
 
 		if(count%this.sig==0 && vely==0 && newSig!=this.sig){
 			this.sig = newSig;
@@ -36,8 +42,9 @@ function Baton() {
 		}
 
 		//BOUNCING
-		if(this.y>height-rad-10){
-			vely=-vely;
+		if(this.y>height-rad-20){
+			vely*=-1;
+			click.play();
 			count++;
 		}else{
 			vely-=this.inc;
@@ -45,10 +52,10 @@ function Baton() {
 
 		if(this.sig==4){
 			if(count%this.sig==1||count%this.sig==3){
-				this.x-=4;
+				this.x-=width/100;
 			}
 			if(count%this.sig==2){
-				this.x+=8;
+				this.x+=width/50;
 			}
 		}
 		if(this.sig==3){
@@ -73,13 +80,38 @@ function Baton() {
 	}
 
 	this.display = function(){
-		background(255);
-		stroke(1);
+		background('#f6f6f6');
+		displayLine(this.x, this.y);
+		//stroke(1);
+		//line(0, height-20, width, height-20);
+		noStroke();
 		fill('black');
-		line(0, height-20, width, height-20);
 		if(count%this.sig==1)
 			fill('red');
-		noStroke();
-		ellipse(this.x, this.y, rad);
+		ellipse(this.x, this.y, 2*rad);
+	}
+
+	function displayLine(x,y) {
+		stroke(2);
+		noFill();
+		beginShape();
+		curveVertex(0, height-60);
+		curveVertex(0, height-60);
+		if(y+rad>height-60){
+			if(x<width/4){
+				vertex(x, y+rad);
+				curveVertex((width+x)/2, y+rad-5);
+			}else if(x>3*width/4){
+				curveVertex(x/2, y+rad-5);
+				vertex(x, y+rad);
+			}else{
+				vertex(x, y+rad);
+			}
+		}else{
+			curveVertex(x, height-60);
+		}
+		curveVertex(width, height-60);
+		curveVertex(width, height-60);
+		endShape();
 	}
 };
